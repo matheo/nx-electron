a9aaeff3f87c13ef5e7d841c0b9c9b16fd221a39
Mateo Tibaquirá <nestormateo@gmail.com>
2019-09-25 14:27:45 -0500

    feat(angular): display some node versions in the app

--- apps/desktop/angular/src/app/app.component.ts
+++ apps/desktop/angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'desktop-angular';
+  versions = process.versions as any;
 }
